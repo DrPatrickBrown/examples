31d63aea7844380ea49dacb8cf7e90f6857a2236
DrPatrickBrown <DrPatrickBrown@btinternet.com>
2019-02-09 20:22:30 +0000

    empty components

---
+++ src/AxiosView/SkillsListContainer.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import SkillsListPresenter from "./SkillsListPresenter.jsx";
+import PropTypes from 'prop-types';
+
+// 
+//
+// 
+
+class SkillsListContainer extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+}
+
+export default SkillsListContainer;
---
+++ src/AxiosView/SkillsListPresenter.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+// 
+//
+// 
+
+class SkillsListPresenter extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div>
+			</div>
+		);
+	}
+
+}
+
+SkillsListPresenter.propTypes = {
+
+}
+
+export default SkillsListPresenter;
